7b8ab1a744e9960d13246a2684d977aea31933a8
tetsuoishikawa <i.tetsuo3003@gmail.com>
2024-12-18 16:41:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import base64
 
 # Streamlit app title
-st.title("カスタムGiNZA:NER抽出＆マスク文章生成")
+st.title("カスタムGiNZA NER抽出＆マスク文章生成")
 
 # GiNZAモデルの読み込み
 nlp = spacy.load("ja_ginza")
